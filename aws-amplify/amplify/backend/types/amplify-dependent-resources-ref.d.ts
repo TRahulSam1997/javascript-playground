48966847d610a3354b4582731e87a835b7f90f01
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-27 22:42:37 +1000

    backend api added

--- aws-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ aws-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "awsamplify4d819912": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apidfa30092": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
